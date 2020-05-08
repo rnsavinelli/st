a4cd13252b2f9bcc928d554d12eeb0cdbec247fe
R. Nicolás Savinelli <rsavinelli@est.frba.utn.edu.ar>
2020-05-07 23:44:46 -0300

    Added contrast

--- config.h
+++ config.h
@@ -83,28 +83,35 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.955555;
+float alpha = 0.98;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-    [0] = "#1d2021", /* hard contrast: #1d2021 / soft contrast: #32302f */
-    [1] = "#cc241d", /* red     */
-    [2] = "#98971a", /* green   */
-    [3] = "#d79921", /* yellow  */
-    [4] = "#458588", /* blue    */
-    [5] = "#b16286", /* magenta */
-    [6] = "#689d6a", /* cyan    */
-    [7] = "#a89984", /* white   */
-      /* 8 bright colors */
-    [8]  = "#928374", /* black   */
-    [9]  = "#fb4934", /* red     */
-    [10] = "#b8bb26", /* green   */
-    [11] = "#fabd2f", /* yellow  */
-    [12] = "#83a598", /* blue    */
-    [13] = "#d3869b", /* magenta */
-    [14] = "#8ec07c", /* cyan    */
-    [15] = "#ebdbb2", /* white   */
+	[0] = "#000000", /* Black   */
+	[1] = "#D40C0C", /* Red	    */
+	[2] = "#2AA94E", /* Green   */
+	[3] = "#FFFF55", /* Yellow  */
+	[4] = "#4979D9", /* Blue    */
+	[5] = "#AA00AA", /* Magenta */
+	[6] = "#00AAAA", /* Cyan    */
+	[7] = "#FFFFFF", /* White   */
+
+	/* 8 bright colors */
+	[8]  = "#E0E0E0", /* Black   */
+	[9]  = "#FF5D5D", /* Red     */
+	[10] = "#49BF5E", /* Green   */
+	[11] = "#FCFC94", /* Yellow  */
+	[12] = "#3F9BB6", /* Blue    */
+	[13] = "#FF55FF", /* Magenta */
+	[14] = "#55FFFF", /* Cyan    */
+	[15] = "#FFFFFF", /* White   */
+
+	[255] = 0,
+
+	/* more colors can be added after 255 to use with DefaultXX */
+	[256] = "#000000", /* Background */
+	[257] = "#FCFCFC", /* Foreground */
 };
 
 /*
@@ -112,10 +119,10 @@ static const char *colorname[] = {
  * foreground, background, cursor
  */
 
-unsigned int defaultfg = 15;
-unsigned int defaultbg = 0;
-static unsigned int defaultcs = 15;
-static unsigned int defaultrcs = 0;
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+static unsigned int defaultcs = 257;
+static unsigned int defaultrcs = 256;
 
 /*
  * Default shape of cursor
